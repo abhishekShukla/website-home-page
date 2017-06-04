6248b7dbde15d03c9493d88e95824e6d048927df
Abhishek Shukla Ravishankara <abhishek.shukla.ravi@gmail.com>
2017-06-04 18:02:07 -0400

    first commit

---
+++ src/main/java/com/abhi/websitehomepage/WebsiteHomePageApplication.java
@@ -0,0 +1,12 @@
+package com.abhi.websitehomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteHomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteHomePageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/abhi/websitehomepage/WebsiteHomePageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abhi.websitehomepage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsiteHomePageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
